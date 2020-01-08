35cb95f3a54a4a98081bc39457090c7519bb0265
Alfonso Reyes <arc980103@gmail.com>
2020-01-08 00:16:52 -0700

    :triangular_flag_on_post: Deploy script should add CNAME file

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,30 @@
+
 #!/bin/sh
 
-# If a command fails then the deploy stops
-set -e
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-
-# Add changes to git.
-cd public && git add --all
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
-git push origin gh-pages
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cp ./CNAME public 
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+#echo "Pushing to github"
+#git push --all
