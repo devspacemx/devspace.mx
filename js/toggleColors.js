90b6220ecd4ead611d0f3e02288dc07802052b86
Alfonso Reyes <arc980103@gmail.com>
2020-01-20 13:56:13 -0700

    :rocket: Publishing to gh-pages (publish.sh)

--- js/toggleColors.js
+++ js/toggleColors.js
@@ -10,40 +10,22 @@ let moonMask = document.getElementById("moonMask");
 initAnimation();
 
 function initAnimation() {
-  if (localStorage.getItem("isLight") === "true") {
-    moonMask.style.top = "-8px";
-    moonMask.style.right = "-5px";
-    moonOrSun.style.transform = "scale(1)";
-    sunRays.style.transform = "scale(0.1)";
-  } else {
+  if (localStorage.getItem("isDark") === "true") {
     moonMask.style.top = "-25px";
     moonMask.style.right = "-15px";
     moonOrSun.style.transform = "scale(0.5)";
     sunRays.style.transform = "scale(0.6)";
+  } else {
+    moonMask.style.top = "-8px";
+    moonMask.style.right = "-5px";
+    moonOrSun.style.transform = "scale(1)";
+    sunRays.style.transform = "scale(0.1)";
   }
 }
 
 function toggleColors() {
-  if (localStorage.getItem("isLight") === "true") {
-    makeDark();
-
-    moonMask.classList.add("mask-to-sun-animation");
-    moonOrSun.classList.add("to-sun-animation");
-    sunRays.classList.add("expand-rays");
-
-    setTimeout(function() {
-      sunRays.classList.remove("expand-rays");
-    }, 500);
-    setTimeout(function() {
-      moonMask.classList.remove("mask-to-sun-animation");
-      moonOrSun.classList.remove("to-sun-animation");
-      initAnimation();
-    }, 400);
-
-    localStorage.setItem("isLight", "false");
-  } else {
+  if (localStorage.getItem("isDark") === "true") {
     makeLight();
-
     moonMask.classList.add("mask-to-moon-animation");
     moonOrSun.classList.add("to-moon-animation");
     sunRays.classList.add("contract-rays");
@@ -57,38 +39,27 @@ function toggleColors() {
       sunRays.classList.remove("contract-rays");
       initAnimation();
     }, 400);
+    localStorage.setItem("isDark", "false");
+  } else {
+    makeDark();
+    moonMask.classList.add("mask-to-sun-animation");
+    moonOrSun.classList.add("to-sun-animation");
+    sunRays.classList.add("expand-rays");
 
-    localStorage.setItem("isLight", "true");
+    setTimeout(function() {
+      sunRays.classList.remove("expand-rays");
+    }, 500);
+    setTimeout(function() {
+      moonMask.classList.remove("mask-to-sun-animation");
+      moonOrSun.classList.remove("to-sun-animation");
+      initAnimation();
+    }, 400);
+
+    localStorage.setItem("isDark", "true");
   }
 }
 
 function makeDark() {
-  bodyElement.style.setProperty("color", "#fff");
-  bodyElement.style.setProperty("--primary", "#fff");
-  bodyElement.style.setProperty("--secondary", "#fff");
-  bodyElement.style.setProperty("--grey", "#8b8b97");
-  bodyElement.style.setProperty("--background-color", "#111216");
-  bodyElement.style.setProperty("--accent", "#E9DAAC");
-  bodyElement.style.setProperty("--hover", "rgba(255, 255, 255, 0.07)");
-  bodyElement.style.setProperty("--articleText", "#fff");
-  bodyElement.style.setProperty("--track", "rgba(255, 255, 255, 0.3)");
-  bodyElement.style.setProperty("--progress", "#fff");
-  bodyElement.style.setProperty("--card", "#1D2128");
-  bodyElement.style.setProperty("--error", "#EE565B");
-  bodyElement.style.setProperty("--success", "#46B17B");
-  bodyElement.style.setProperty("--errorBackground", "rgba(238, 86, 91, 0.1)");
-  bodyElement.style.setProperty(
-    "--horizontalRule",
-    "rgba(255, 255, 255, 0.15)"
-  );
-  bodyElement.style.setProperty(
-    "--inputBackground",
-    "rgba(255, 255, 255, 0.07)"
-  );
-  bodyElement.style.setProperty("--tooltip", "#000");
-}
-
-function makeLight() {
   bodyElement.style.setProperty("--primary", "");
   bodyElement.style.setProperty("--secondary", "");
   bodyElement.style.setProperty("--grey", "");
@@ -104,5 +75,24 @@ function makeLight() {
   bodyElement.style.setProperty("--errorBackground", "");
   bodyElement.style.setProperty("--horizontalRule", "");
   bodyElement.style.setProperty("--inputBackground", "");
+  bodyElement.style.setProperty("--tooltip", "");
+}
+
+function makeLight() {
+  bodyElement.style.setProperty("--primary", "#222222");
+  bodyElement.style.setProperty("--secondary", "#73737d");
+  bodyElement.style.setProperty("--grey", "#44444b");
+  bodyElement.style.setProperty("--background-color", "#fafafa");
+  bodyElement.style.setProperty("--accent", "#6166dc");
+  bodyElement.style.setProperty("--hover", "rgba(0, 0, 0, 0.07)");
+  bodyElement.style.setProperty("--articleText", "#08080b");
+  bodyElement.style.setProperty("--track", "rgba(8, 8, 11, 0.3)");
+  bodyElement.style.setProperty("--progress", "#000");
+  bodyElement.style.setProperty("--card", "#fff");
+  bodyElement.style.setProperty("--error", "#ee565b");
+  bodyElement.style.setProperty("--success", "#46b17b");
+  bodyElement.style.setProperty("--errorBackground", "rgba(238, 86, 91, 0.1)");
+  bodyElement.style.setProperty("--horizontalRule", "rgba(8, 8, 11, 0.15)");
+  bodyElement.style.setProperty("--inputBackground", "rgba(0, 0, 0, 0.05)");
   bodyElement.style.setProperty("--tooltip", "lightgrey");
 }
